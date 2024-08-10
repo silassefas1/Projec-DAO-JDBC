041fd48699d588843b3945c289da032b6be338c5
Silas Sefas <silassefas23@gmail.com>
2024-08-10 11:45:37 -0300

    Department Entity Class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		//teste de funcionalidade
+		Department object = new Department(1, "Books");
+		System.out.println(object);
+
+	}
+
+}
