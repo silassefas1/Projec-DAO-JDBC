66125cef414469c40e2d2bc02260275032499ada
Silas Sefas <silassefas23@gmail.com>
2024-08-12 15:49:16 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,9 @@ public class Program {
 		//teste de funcionalidade
 		Department object = new Department(1, "Books");
 		System.out.println(object);
-
+		
+		Seller seller = new Seller(21,"Bob", "bob@gmaiol.com", new Date(), 3000.0, object);
+		System.out.println(seller);
 	}
 
 }
