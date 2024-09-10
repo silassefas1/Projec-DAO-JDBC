28d3bf6a6f73a954b7aabe583abaa88d7737ab70
Silas Sefas <silassefas23@gmail.com>
2024-09-10 09:03:43 -0300

    Update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,12 +11,19 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 	
-		System.out.println("=== TEST 1: Seller FindById ===");
+		System.out.println("=== TEST 1: Department Insert ===");
 		Department newDepartment = new Department(null, "Customer Service");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New Department Id: "+ newDepartment.getId());
 		
+		System.out.println("=== TEST 2: Department Update ===");
+		Department updateDepartment = new Department(8, "Play Center");
+		departmentDao.update(updateDepartment);
+		System.out.println("Department Updated!");
+		
 
+		DB.closeConnection();
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -54,7 +54,25 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department object) {
-		// TODO Auto-generated method stub
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			preparedStatement = connection.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE "
+					+ "Department.Id = ?");
+			preparedStatement.setString(1, object.getName());
+			preparedStatement.setInt(2, object.getId());
+			
+			preparedStatement.executeUpdate();
+			
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(preparedStatement);
+		}
 		
 	}
 
